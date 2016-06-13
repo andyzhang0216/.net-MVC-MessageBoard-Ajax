d1fd87f67da0069079dd3ac8850cc5068ba06cce
chilin99999 <chilin99999@gmail.com>
2016-06-13 15:11:51 +0800

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,7 @@
-﻿using MessageBoard.Models;
+using MessageBoard.Models;
 using PagedList;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MessageBoard.Controllers
@@ -12,35 +10,30 @@ namespace MessageBoard.Controllers
     {
         DatabaseModel db = new DatabaseModel();
 
-        public ActionResult Index(int page = 1)
+        public ActionResult Index()
         {
-            int currentPage = page < 1 ? 1 : page;
-            var getData = db.Messages.OrderByDescending(x => x.Time);
-            var messages = getData.ToPagedList(currentPage, 6);
-
-            return View(messages);
+            return View();
         }
 
         [HttpPost]
-        public PartialViewResult AjaxPost(Messages input, int page = 1)
+        public ActionResult NewPost(Messages input)
         {
-            Messages data = new Messages()
+            if (input.Title != null && input.Content != null)
             {
-                Title = input.Title,
-                Content = input.Content,
-                Time = DateTime.Now
-            };
-            db.Messages.Add(data);
-            db.SaveChanges();
+                Messages data = new Messages()
+                {
+                    Title = input.Title,
+                    Content = input.Content,
+                    Time = DateTime.Now
+                };
+                db.Messages.Add(data);
+                db.SaveChanges();
+            }
 
-            int currentPage = page < 1 ? 1 : page;
-            var getData = db.Messages.OrderByDescending(x => x.Time);
-            var messages = getData.ToPagedList(currentPage, 6);
-
-            return PartialView("_MessageList", messages);
+            return RedirectToAction("RenderPosts");
         }
 
-        public PartialViewResult AjaxPost(int page = 1)
+        public PartialViewResult RenderPosts(int page = 1)
         {
             int currentPage = page < 1 ? 1 : page;
 
